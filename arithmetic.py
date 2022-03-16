323e253edf1943bd23bbe9eda44f954263388855
Caroline Sitter <carolinesitter@Carolines-iMac.attlocal.net>
2022-03-16 14:18:31 -0500

    Implement X+ Feature

--- arithmetic.py
+++ arithmetic.py
@@ -42,3 +42,8 @@ def mod(num1, num2):
     """Return the remainder of num1 / num2."""
 
     return num1 % num2
+
+def add_mult(num1, num2, num3):
+    """Get the sum of num1 and num 2, then multiple sum with num3."""
+
+    return (num1 + num2) * num3
